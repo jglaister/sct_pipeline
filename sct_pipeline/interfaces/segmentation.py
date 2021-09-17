5283dbc61f4207b1ecdf71a98659f1496bb98fd4
jglaister <jeff.glaister@gmail.com>
2021-09-17 12:15:50 -0400

    Debug

--- sct_pipeline/interfaces/segmentation.py
+++ sct_pipeline/interfaces/segmentation.py
@@ -135,7 +135,7 @@ class CreateMask(CommandLine):
         if isdefined(self.inputs.output_filename):
             outputs['mask_file'] = os.path.abspath(self.inputs.output_filename)
         else:
-            outputs['mask_file'] = os.path.abspath(split_filename(self.inputs.spine_segmentation)[1] + '_mask.nii.gz')
+            outputs['mask_file'] = os.path.abspath(split_filename(self.inputs.input_image)[1] + '_mask.nii.gz')
         return outputs
 
 
