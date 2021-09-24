8bdd94646c7931637fdfed59aef4059125c4f7b1
jglaister <jeff.glaister@gmail.com>
2021-09-24 13:44:25 -0400

    Debug

--- sct_pipeline/workflows/processing.py
+++ sct_pipeline/workflows/processing.py
@@ -255,7 +255,7 @@ def create_spinalcord_mtr_workflow(scan_directory, patient_id=None, scan_id=None
         wf.connect(process_seg, 'output_csv', export_csa_metric, 'in_file')
 
     if compute_avggmwm:
-        export_avggmwm = pe.Node(io.ExportFile(), name='export_csa_metric')
+        export_avggmwm = pe.Node(io.ExportFile(), name='export_avggmwm')
         export_avggmwm.inputs.check_extension = True
         export_avggmwm.inputs.clobber = True
         export_avggmwm.inputs.out_file = out_file_base + '_avg_GM_WM_MTR.csv'
